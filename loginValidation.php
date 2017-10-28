408acca0eac957d68c9e1c386ba7a388094b5156
Daenise <31722262+Daenise@users.noreply.github.com>
2017-10-28 15:20:18 +0800

    updateLogin

--- loginValidation.php
+++ loginValidation.php
@@ -1,25 +1,26 @@
 <?php
 session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "helpfit";
+$con = new mysqli($servername, $username, $password, $dbname);
+
 if(isset($_POST['submit']))
 {
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "helpfit";
- $con = new mysqli($servername, $username, $password, $dbname);
+
  if (!$con) {
   die("Could not connect to database.");
   }
  echo "Database connected."."</br>";
-
-
  $username = $_POST['username'];
  $password = $_POST['password'];
 
 
  if($username!='')
  {
-   $sql = "select * from members WHERE username='$username' and password='$password'";
+   $sql = "SELECT * FROM members WHERE username='$username' and password='$password'";
    $result = mysqli_query($con, $sql);
 
    if (mysqli_num_rows($result) > 0)
