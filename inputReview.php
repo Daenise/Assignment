dde166df037e9096f685d393114fcccb754ba938
B1601563_CT <celineteh2011@gmail.com>
2017-10-30 23:38:10 +0800

    small changes

--- inputReview.php
+++ inputReview.php
@@ -1,8 +1,39 @@
 // inputReview.php
 
 <?php
-  $rating = $_POST["trainerRating"];
-  $comments = $_POST["reviewComments"];
+  // Connect to database
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "helpfit";
+  $con = new mysqli($servername, $username, $password, $dbname);
+
+  if (!$con) {
+   die("Could not connect to database.");
+   }
+  echo "Database connected."."</br>";
+
+  // Store values from user input in review form
+  $rating = $_POST['trainerRating'];
+  $comments = $_POST['reviewComments'];
+  $sessionID = $_POST['sessionID'];
+
+  // Add record
+  $sql_addReview = "INSERT INTO  reviews (rating, comments, sessionID) VALUES ('$rating','$comments','$sessionID')";
+
+  $result_addReview = mysqli_query($con, $sql_addReview);
+
+ if ($result_addReview) {
+   echo "Review successfully added.";
+   echo "Redirecting back to training history page";
+   header("Refresh: 5; url= memberTrainingHist.html");
+ }
+ else {
+    echo "Error submitting a review : " . mysqli_error($con);
+    mysqli_error($con);
+  }
+
+ mysqli_close($con);
 
 
  ?>
--- loginValidation.php
+++ loginValidation.php
@@ -46,4 +46,5 @@ if(isset($_POST['submit']))
  }
 }
 
+mysqli_close($con);
 ?>
