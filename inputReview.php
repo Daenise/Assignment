a43dbbeb08d5b222131c96a6369d6b69bcca3f86
B1601563_CT <celineteh2011@gmail.com>
2017-10-31 00:09:03 +0800

    review milestone!

--- inputReview.php
+++ inputReview.php
@@ -1,6 +1,7 @@
-// inputReview.php
+<!-- inputReview.php -->
 
 <?php
+  session_start();
   // Connect to database
   $servername = "localhost";
   $username = "root";
@@ -17,14 +18,16 @@
   $rating = $_POST['trainerRating'];
   $comments = $_POST['reviewComments'];
   $sessionID = $_POST['sessionID'];
+  $sessionTrainer = $_POST['sessionTrainer'];
+  $theMember = $_SESSION['user'];
 
   // Add record
-  $sql_addReview = "INSERT INTO  reviews (rating, comments, sessionID) VALUES ('$rating','$comments','$sessionID')";
+  $sql_addReview = "INSERT INTO  reviews (rating, comments, sessionID, sessionTrainer, reviewedBy) VALUES ('$rating','$comments','$sessionID', '$sessionTrainer', '$theMember')";
 
   $result_addReview = mysqli_query($con, $sql_addReview);
 
  if ($result_addReview) {
-   echo "Review successfully added.";
+   echo "Review successfully added. <br>";
    echo "Redirecting back to training history page";
    header("Refresh: 5; url= memberTrainingHist.html");
  }
